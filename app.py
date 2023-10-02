87520ec1994a51f66dc2bc48a9c4f940d9fef2f2
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-10-02 10:40:37 +0800

    add: sidebar file

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
 
 @app.route("/")
-@app.route("/index")
+@app.route("/login")
 def index():
-	return render_template("index.html")
+	return render_template("sidebar.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
