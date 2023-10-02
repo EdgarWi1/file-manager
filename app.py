bf2f451e1ceed660c6f0ec3e4c5ff55fdea3f868
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-10-02 17:19:46 +0800

    add: dashboard.html with included sidebar

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
 
 @app.route("/")
-@app.route("/login")
+# @app.route("/login")
+# def index():
+# 	return render_template("login.html")
+
+# @app.route("/sidebar")
+# def index():
+# 	return render_template("assets/sidebar.html")
+
+@app.route("/dashboard")
 def index():
-	return render_template("sidebar.html")
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
