d393e1b32b02cf15b83f298e81de32ed81cc6dea
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-10-30 13:31:50 +0800

    fix

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
