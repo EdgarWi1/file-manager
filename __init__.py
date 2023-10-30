d80948910556e4bc1ce59712be14ccf1e02b1382
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-10-30 13:12:18 +0800

    fix

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
---
+++ config.py
@@ -0,0 +1,3 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI ="mysql+pymysql://root:''@localhost:3306/dblatihan"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
