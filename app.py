d93f5556174bf697b031f21c5f32c0dfa39a1491
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-11-09 07:15:55 +0800

    fix

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route("/")
 # @app.route("/login")
