423703c3c7e270769157c9ef8f1df6fa71c3d634
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-11-15 14:09:05 +0800

    fix/login

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
 # db = SQLAlchemy(app)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def login():
+    
+    if request.method == 'POST':
+        return redirect(url_for('dashboard.html'))
+    
     return render_template('login.html')
 
-@app.route("/dashboard")
+@app.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
     return render_template('dashboard.html')
 
