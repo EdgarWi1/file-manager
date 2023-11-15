dc2fdc1d3a322531897a89f44fb515718c65edeb
EdgarWi1 <edgarsilvanus12@gmail.com>
2023-11-15 10:52:31 +0800

    fix/login

--- app.py
+++ app.py
@@ -5,17 +5,12 @@ app = Flask(__name__, template_folder='src/templates', static_folder='src/static
 # db = SQLAlchemy(app)
 
 @app.route("/")
-# @app.route("/login")
-# def index():
-# 	return render_template("login.html")
-
-# @app.route("/sidebar")
-# def index():
-# 	return render_template("assets/sidebar.html")
+def login():
+    return render_template('login.html')
 
 @app.route("/dashboard")
-def index():
+def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True) 
